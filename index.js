ccdbd954cc868476b87f399bc91cb34517217277
Coshiloco <pablohurtadohg@gmail.com>
2024-07-15 19:56:13 +0200

    Proceso de Formulario

---
+++ index.js
@@ -0,0 +1,19 @@
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init('YOUR_PUBLIC_KEY');
+})();
+
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+        
+        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', this)
+            .then(function() {
+                console.log('SUCCESS!');
+                alert('Mensaje enviado con éxito!');
+            }, function(error) {
+                console.log('FAILED...', error);
+                alert('Error al enviar el mensaje. Por favor, intenta de nuevo.');
+            });
+    });
+}
