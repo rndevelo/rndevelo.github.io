c8aebd9b6371c963b2c1a0a22921527ca9cd4188
Coshiloco <pablohurtadohg@gmail.com>
2024-07-16 19:14:54 +0200

    ya funciona

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-
-
 (function() {
-    // Inicialización de emailjs usando la variable de entorno
     emailjs.init('mI62WskSC-u4po9oO');
 })();
 
@@ -9,23 +6,13 @@ window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         
-        var userEmail = document.getElementById('user-email').value;
-        var userMessage = document.getElementById('user-message').value;
-
-        var templateParams = {
-            from_name: 'PabloPruebas',
-            to_name: 'User',
-            reply_to: userEmail,
-            message: userMessage
-        };
-        
         emailjs.sendForm('service_ss1po4w', 'template_g32gu89', this)
-            .then(function() {
-                console.log('SUCCESS!');
-                alert('Mensaje enviado con éxito!');
+            .then(function(response) {
+                console.log('SUCCESS!', response.status, response.text);
+                alert('Message sent successfully!');
             }, function(error) {
                 console.log('FAILED...', error);
-                alert('Error al enviar el mensaje. Por favor, intenta de nuevo.');
+                alert('Error sending the message. Please try again.');
             });
     });
 };
